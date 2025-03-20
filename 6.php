5732c8da0c76fb416615e8ad5eafd484772232bc
dahaia1 <19818603997@163.com>
2025-03-20 10:11:31 +0800

    Add files via upload

---
+++ 6.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+$a=6;
+$b=3;
+$c=($a<10 && $b>1);
+var_dump($c);
+$c=($a==$b || $b==1);
+var_dump($c);
+$c=($a>10 xor $b<1);
+var_dump($c);
+$c=!($a == $b);
+var_dump($c);
+$a= true;
+$b= false;
+if ($a || $b){
+	echo "条件为真";
+} else {
+	echo "条件为假";
+}
+?>
