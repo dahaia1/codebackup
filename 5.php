a2d86b2409052819f3760a0f05a8f8d8c1ca9b0d
dahaia1 <19818603997@163.com>
2025-03-20 09:50:52 +0800

    20240320

---
+++ 5.php
@@ -0,0 +1,22 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+$x=5;
+$y="5";
+var_dump($x == $y);
+var_dump($x === $y);
+var_dump($x != $y);
+var_dump($x !== $y);
+$a=5;
+$b=8;
+var_dump($a >= $b);
+var_dump($a < $b);
+if ($x === $y){
+	echo "它们完全相同";
+} else {
+	echo "它们不完全相同";
+}
+$c=84;
+$r=$c >= 60 ? "及格" : "不及格";
+echo "<br>";
+echo $r;
+?>
