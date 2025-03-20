7b740f8526862b352b34e465e56fffd069cb16f3
dahaia1 <19818603997@163.com>
2025-03-20 09:14:14 +0800

    Add files via upload
    20250320实训1

---
+++ 4.php
@@ -0,0 +1,18 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+$a=9;
+$b=12;
+echo $a&$b;
+echo "<br>";
+echo $a | $b;
+echo "<br>";
+echo $a ^ $b;
+echo "<br>";
+echo -$a;
+echo "<br>";
+echo ~$a;
+echo "<br>";
+echo $a << 2;
+echo "<br>";
+echo $b >> 2;
+?>
