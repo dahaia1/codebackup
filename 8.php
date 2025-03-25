630545833c123aba9c9e53dae24647f3b67f0072
dahaia1 <19818603997@163.com>
2025-03-25 10:08:10 +0800

    Add files via upload

---
+++ 8.php
@@ -0,0 +1,31 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+date_default_timezone_set ("PRC");
+//if...esleif...
+$day = date("D");
+if ($day == "Fri") {
+echo "Have a nice weekend!"."<br>";
+} elseif ($day == "Sun") {
+	echo "Have a nice Sunday"."<br>";
+} else {
+	echo "Have a nice day! "."<br>";
+}
+//switch
+switch ($day) {
+case "Mon":
+echo "今天是星期一";
+break;
+case"Tue":
+echo "今天是星期二";
+break;
+case "Wed":
+echo "今天是星期三";
+break;
+case"Thu":
+echo "今天是星期四";break;
+case "Fri":
+echo "今天是星期五";break;
+default:
+echo "今天休息。";
+}
+?>
