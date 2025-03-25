00bc783b1b284b6d3049ee29c88f77154a84a86f
dahaia1 <19818603997@163.com>
2025-03-25 09:12:37 +0800

    20250325

---
+++ 7.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+$a=3*8%5;
+echo $a."<br>";
+$b=true?0:true?1:2;
+echo $b."<br>";
+$x=1;
+$y=2;
+$x=$y+=3;
+echo $x."<br>";
+$a1=true or true and false;
+echo $a1."<br>";
---
+++ a.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+$a=3*8%5;
+var_dump($a);
+echo $a."<br>";
+$b=true?0:true?1:2;
+var_dump($b);
+echo $b."<br>";
+$x=1;
+$y=2;
+$x=$y+=3;
+var_dump($x);
+echo $x."<br>";
+$a1=true or true and false;
+var_dump($a1);
+echo $a1."<br>";
